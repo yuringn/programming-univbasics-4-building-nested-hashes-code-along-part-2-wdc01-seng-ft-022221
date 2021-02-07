fcb7b7e276b74e57480f933fa3d1ca8cf04b68f3
Yuri Ng <yuringuyen312@gmail.com>
2021-02-07 03:36:39 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -30,7 +30,7 @@ epic_tragedy[:montague][:heroine][:status] = "dead"
 
 
   # After your code, the following code will return the altered hash. This method must only return the modified epic_tragedy hash
-  epic_tragedy
+p  epic_tragedy
 end
 
 def add_characters
